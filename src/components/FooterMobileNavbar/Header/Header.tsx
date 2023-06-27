accc9dbd5b256a9ef4695d4a26f7f80499133ac0
Omid-Neshati <tchandnofame@gmail.com>
2023-06-27 17:15:33 +0330

    create header & add it to layout

---
+++ src/components/FooterMobileNavbar/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { Profile, ShoppingCart } from 'iconsax-react';
+import Link from 'next/link';
+import Searchbar from './Searchbar/Searchbar';
+import Logo from './Logo/Logo';
+import Navbar from './Navbar/Navbar';
+
+function Header() {
+  return (
+    <nav className="fixed inset-x-0 top-0 justify-center hidden pt-2 shadow-md sm:flex">
+      <div className="flex flex-col justify-center w-full max-w-screen-xl ">
+        <div className="flex justify-around pb-2 border-b border-gray-400">
+          <Logo />
+          <Searchbar />
+          <div className="flex justify-center w-1/5 gap-2 p-2">
+            <Link
+              href="/profile"
+              className="grid w-12 h-12 transition-all duration-150 rounded-full hover:bg-gray-200 place-content-center">
+              <Profile />
+            </Link>
+            <Link
+              href="/cart"
+              className="grid w-12 h-12 transition-all duration-150 rounded-full hover:bg-gray-200 place-content-center">
+              <ShoppingCart />
+            </Link>
+          </div>
+        </div>
+        <Navbar />
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/FooterMobileNavbar/Header/Searchbar/Searchbar.tsx
@@ -0,0 +1,12 @@
+import { SearchNormal1 } from 'iconsax-react';
+
+function Searchbar() {
+  return (
+    <div className="flex items-center w-3/5 gap-2 px-4 py-2 bg-gray-100 rounded-md">
+      <SearchNormal1 className="text-gray-200" />
+      <input className="w-full bg-transparent outline-none" placeholder="جستجو ..." />
+    </div>
+  );
+}
+
+export default Searchbar;
