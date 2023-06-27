7ebac7b50ad90ae9654af322a901b2fb5fb500e9
Omid-Neshati <tchandnofame@gmail.com>
2023-06-28 01:37:34 +0330

    add herobanner and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HeroBanner from '@/components/HeroBanner/HeroBanner';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <HeroBanner />
+    </main>
+  );
 }
---
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+function HeroBanner() {
+  return (
+    <div className="sm:h-[80vh] grid sm:grid-cols-3 sm:grid-rows-2 gap-3 max-w-screen-xl">
+      <Link href="" className="relative block h-full col-span-2 row-span-2 ">
+        <Image src="/images/img1.png" alt="img1" fill className="rounded-md" />
+      </Link>
+      <Link href="" className="relative block h-full col-span-1 row-span-1 ">
+        <Image src="/images/img2.png" alt="img2" fill className="rounded-md" />
+      </Link>
+      <Link href="" className="relative block h-full col-span-1 row-span-1 ">
+        <Image src="/images/img3.png" alt="img3" fill className="rounded-md" />
+      </Link>
+    </div>
+  );
+}
+
+export default HeroBanner;
