458b083ee4ee02703c5a54f6cef14aa66e11e3c5
Omid-Neshati <65368035+omidneshati@users.noreply.github.com>
2023-06-29 05:41:07 +0330

    Herobanner (#12)
    * remove useless svgs
    
    * create header & add it to layout
    
    * mis addressing & move files
    
    * fix import path
    
    * add herobanner and images
    
    * change images
    
    * herobanner
    
    * fix style & decrease height
    
    * change h to min-h and desktop min-h screen & add p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroBanner from '@/components/HeroBanner/HeroBanner';
 export default function Home() {
   return (
     <main className="flex justify-center">
-      <div className="flex flex-col justify-center w-full max-w-screen-xl">
+      <div className="flex flex-col justify-center w-full max-w-screen-xl p-3">
         <HeroBanner />
       </div>
     </main>
