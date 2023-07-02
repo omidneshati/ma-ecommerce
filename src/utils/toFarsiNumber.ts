b5f89cc3bd1b8bc6c329283a378f44d5051a8dca
Omid-Neshati <tchandnofame@gmail.com>
2023-07-02 11:59:32 +0330

    remove console.log

--- src/utils/toFarsiNumber.ts
+++ src/utils/toFarsiNumber.ts
@@ -1,8 +1,5 @@
 export default function toFarsiNumber(n: number) {
   const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
 
-  return n.toString().replace(/\d/g, (x) => {
-    console.log(x);
-    return farsiDigits[Number(x)];
-  });
+  return n.toString().replace(/\d/g, (x) => farsiDigits[Number(x)]);
 }
