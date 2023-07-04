cc1535d1336969a78e1f0ae510d3aeba77e060c0
Omid-Neshati <tchandnofame@gmail.com>
2023-07-04 15:41:27 +0330

    offer section done

--- src/components/OfferSection/OfferSection.tsx
+++ src/components/OfferSection/OfferSection.tsx
@@ -3,14 +3,22 @@ import ProductsSlider from './ProductSlider/ProductsSlider';
 
 async function OfferSection() {
   try {
-    const productList = await getProduct({ limit: 10 });
+    const bestSellerList = await getProduct({ limit: 10 });
+    const NewOffer = await getProduct({ limit: 10, sort: 'desc' });
     return (
-      <div>
-        <ProductsSlider productList={productList} />
+      <div className="flex flex-col gap-3 my-3">
+        <div className="p-3 bg-white shadow-lg">
+          <label className="font-bold">پر فروش ترین‌ها</label>
+          <ProductsSlider productList={bestSellerList} />
+        </div>
+        <div className="p-3 bg-white shadow-lg">
+          <label className="font-bold">‌پیشنهاد شگفت انگیز</label>
+          <ProductsSlider productList={NewOffer} />
+        </div>
       </div>
     );
   } catch (err) {
-    return <div>ERROR</div>;
+    return <div className="flex items-center justify-center text-red-700 text-9xl">ERROR</div>;
   }
 }
 
