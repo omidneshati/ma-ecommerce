6386ff09a2a97776156ddaad1f38e0d177381f13
Omid-Neshati <tchandnofame@gmail.com>
2023-07-11 12:54:24 +0330

    limit width to screen-xl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import OfferSection from '@/components/OfferSection/OfferSection';
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
-      <div className="flex flex-col justify-center w-full max-w-screen-xl p-3">
-        <HeroBanner />
-        <OfferSection />
-        <HomeFooter />
-      </div>
+    <main className="flex flex-col justify-center w-full p-3">
+      <HeroBanner />
+      <OfferSection />
+      <HomeFooter />
     </main>
   );
 }
