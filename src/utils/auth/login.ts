ca07bbdd8a7906bd8b345716f80f14f2d48b697b
Omid-Neshati <tchandnofame@gmail.com>
2023-07-12 20:00:17 +0330

    set user logged in

--- src/utils/auth/login.ts
+++ src/utils/auth/login.ts
@@ -1,6 +1,6 @@
 export default function login({ name, password }: { name: string; password: string }) {
   const storedName: string | null = localStorage.getItem('name');
   const storedPassword: string | null = localStorage.getItem('password');
-  if (name === storedName && password === storedPassword) return true;
-  return false;
+  if (name === storedName && password === storedPassword) localStorage.setItem('loggedIn', 'true');
+  return undefined;
 }
