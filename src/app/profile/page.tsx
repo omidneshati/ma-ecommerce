cc2307562de53b69a6800e993e6185c6a3495586
Omid-Neshati <tchandnofame@gmail.com>
2023-07-14 14:03:18 +0330

    use grid instead of flex add cart and logout buttons change placeholders add read only style add edit button logic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+import { LogoutCurve, ShoppingCart } from 'iconsax-react';
 import useProfile from './useProfile';
 
 function Profile() {
@@ -12,72 +14,92 @@ function Profile() {
     changeNameHandler,
     changePhoneHandler,
     changeAddressHandler,
-    submitHandler
+    submitHandler,
+    logoutHandler
   } = useProfile();
   return (
-    <main>
-      <div></div>
-      <form
-        onSubmit={submitHandler}
-        className="w-full max-w-sm p-8 mx-auto bg-white rounded-md shadow-md">
-        <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="name">
-            نام
-          </label>
-          <input
-            value={inputName}
-            onChange={changeNameHandler}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="text"
-            id="name"
-            name="name"
-            placeholder="John Doe"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="tel">
-            شماره تلفن
-          </label>
-          <input
-            value={inputPhone}
-            onChange={changePhoneHandler}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="tel"
-            id="tel"
-            name="tel"
-            placeholder="john@example.com"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="address">
-            آدرس
-          </label>
-          <input
-            value={inputAddress}
-            onChange={changeAddressHandler}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="text"
-            id="address"
-            name="address"
-            placeholder="خیابان ..."
-          />
-        </div>
-        <div>
-          <button
-            onClick={editableHandler}
-            className="px-4 py-2 text-sm font-bold text-white transition duration-300 bg-red-500 rounded-md hover:bg-red-600"
-            type="submit">
-            {editable ? 'بستن' : 'ویرایش'}
-          </button>
-          {editable && (
+    <main className="flex items-center justify-center m-1 sm:m-0">
+      <div className="grid w-full max-w-xl grid-cols-4 gap-4 sm:grid-cols-5 place-content-center">
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col w-full col-span-4 gap-6 p-6 bg-white rounded-md shadow-md">
+          <div>
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="name">
+              نام
+            </label>
+            <input
+              readOnly={!editable}
+              value={inputName}
+              onChange={changeNameHandler}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none read-only:bg-gray-200 focus:border-indigo-500"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="John Doe"
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="tel">
+              شماره تلفن
+            </label>
+            <input
+              readOnly={!editable}
+              value={inputPhone}
+              onChange={changePhoneHandler}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none read-only:bg-gray-200 focus:border-indigo-500"
+              type="tel"
+              id="tel"
+              name="tel"
+              placeholder="09123456789"
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="address">
+              آدرس
+            </label>
+            <input
+              readOnly={!editable}
+              value={inputAddress}
+              onChange={changeAddressHandler}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none read-only:bg-gray-200 focus:border-indigo-500"
+              type="text"
+              id="address"
+              name="address"
+              placeholder="تهران ..."
+            />
+          </div>
+          <div className="flex">
+            {editable && (
+              <button
+                className="px-4 py-2 text-sm font-bold text-white transition duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600"
+                type="submit">
+                ثبت
+              </button>
+            )}
             <button
-              className="px-4 py-2 text-sm font-bold text-white transition duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600"
-              type="submit">
-              ثبت
+              onClick={editableHandler}
+              type="button"
+              className="px-4 py-2 mr-auto text-sm font-bold text-white transition duration-300 bg-red-500 rounded-md justify-self-end hover:bg-red-600">
+              {editable ? 'بستن' : 'ویرایش'}
             </button>
-          )}
+          </div>
+        </form>
+        <div className="flex col-span-4 gap-2 sm:row-start-1 sm:col-start-5 sm:flex-col">
+          <Link
+            href="/cart"
+            className="flex flex-col items-center justify-center w-1/2 gap-2 p-3 bg-white rounded-md shadow-md sm:w-full sm:h-1/3">
+            <ShoppingCart />
+            سبد خرید
+          </Link>
+          <a
+            href="#"
+            onClick={logoutHandler}
+            className="flex flex-col items-center justify-center w-1/2 gap-2 p-3 bg-white rounded-md shadow-md sm:w-full sm:h-1/3">
+            <LogoutCurve />
+            خروج
+          </a>
         </div>
-      </form>
+      </div>
     </main>
   );
 }
