d70682cfdc87a225f45f6edc698bd72dedb5025f
Omid-Neshati <tchandnofame@gmail.com>
2023-07-14 12:36:59 +0330

    add values

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,17 +3,30 @@
 import useProfile from './useProfile';
 
 function Profile() {
-  const { userName } = useProfile();
+  const {
+    editable,
+    editableHandler,
+    inputName,
+    inputPhone,
+    inputAddress,
+    changeNameHandler,
+    changePhoneHandler,
+    changeAddressHandler,
+    submitHandler
+  } = useProfile();
   return (
     <main>
       <div></div>
-      <form className="w-full max-w-sm p-8 mx-auto bg-white rounded-md shadow-md">
+      <form
+        onSubmit={submitHandler}
+        className="w-full max-w-sm p-8 mx-auto bg-white rounded-md shadow-md">
         <div className="mb-4">
           <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="name">
-            Name
+            نام
           </label>
           <input
-            value={userName}
+            value={inputName}
+            onChange={changeNameHandler}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
             type="text"
             id="name"
@@ -22,46 +35,48 @@ function Profile() {
           />
         </div>
         <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
-            Email
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="tel">
+            شماره تلفن
           </label>
           <input
+            value={inputPhone}
+            onChange={changePhoneHandler}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="email"
-            id="email"
-            name="email"
+            type="tel"
+            id="tel"
+            name="tel"
             placeholder="john@example.com"
           />
         </div>
         <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
-            Password
+          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="address">
+            آدرس
           </label>
           <input
+            value={inputAddress}
+            onChange={changeAddressHandler}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="password"
-            id="password"
-            name="password"
-            placeholder="********"
+            type="text"
+            id="address"
+            name="address"
+            placeholder="خیابان ..."
           />
         </div>
-        <div className="mb-4">
-          <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="confirm-password">
-            Confirm Password
-          </label>
-          <input
-            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-            type="password"
-            id="confirm-password"
-            name="confirm-password"
-            placeholder="********"
-          />
+        <div>
+          <button
+            onClick={editableHandler}
+            className="px-4 py-2 text-sm font-bold text-white transition duration-300 bg-red-500 rounded-md hover:bg-red-600"
+            type="submit">
+            {editable ? 'بستن' : 'ویرایش'}
+          </button>
+          {editable && (
+            <button
+              className="px-4 py-2 text-sm font-bold text-white transition duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600"
+              type="submit">
+              ثبت
+            </button>
+          )}
         </div>
-        <button
-          className="w-full px-4 py-2 text-sm font-bold text-white transition duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600"
-          type="submit">
-          Register
-        </button>
       </form>
     </main>
   );
