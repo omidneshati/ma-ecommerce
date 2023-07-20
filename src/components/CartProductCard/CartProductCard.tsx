c1ce9aae6402b15c80810a8ba3fc95c64623e986
Omid-Neshati <tchandnofame@gmail.com>
2023-07-21 03:08:22 +0330

    rerender cart by changing product quantity

--- src/components/CartProductCard/CartProductCard.tsx
+++ src/components/CartProductCard/CartProductCard.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import Image from 'next/image';
 import ProductType from '@/interfaces/Product/Product';
-import AddToCartBtn from '../AddToCartBtn/AddToCartBtn';
-import RemoveFromCartBtn from '../RemoveFromCart/RemoveFromCartBtn';
+import useCartProductCard from './useCartProductCard';
 
 function CartProductCard({ product, quantity }: { product: ProductType; quantity: number }) {
+  const { addToCartHandler, number, removeFromCartHandler } = useCartProductCard({
+    product,
+    quantity
+  });
+
+  if (number === 0) return null;
+
   return (
     <div className="flex items-center justify-between w-full gap-2 p-2 bg-white rounded-md sm:gap-6 sm:p-6">
       <div className="flex gap-4">
@@ -25,18 +33,18 @@ function CartProductCard({ product, quantity }: { product: ProductType; quantity
       </div>
 
       <div className="flex items-center gap-4">
-        <span className="font-bold sm:text-xl whitespace-nowrap">تعداد: {quantity}</span>
+        <span className="font-bold sm:text-xl whitespace-nowrap">تعداد: {number}</span>
         <div>
-          <AddToCartBtn
+          <button
             className="top-0 right-0 flex items-center justify-center p-2 text-3xl text-white w-14 h-14 bg-green-800/70 "
-            product={product}>
+            onClick={addToCartHandler}>
             +
-          </AddToCartBtn>
-          <RemoveFromCartBtn
+          </button>
+          <button
             className="top-0 right-0 flex items-center justify-center p-2 text-3xl text-white w-14 h-14 bg-red-800/70 "
-            product={product}>
+            onClick={removeFromCartHandler}>
             -
-          </RemoveFromCartBtn>
+          </button>
         </div>
       </div>
     </div>
